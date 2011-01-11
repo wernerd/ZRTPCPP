301f4bf674c8ae8c2a4dde52fa237afdcb055f85
wernerd <wernerd@c26ef401-41b0-48d7-a563-427cd9f8604a>
2011-01-11 18:23:50 +0000

    remove not required 'obsoletes' keyword
    git-svn-id: http://svn.savannah.gnu.org/svn/ccrtp/trunk@674 c26ef401-41b0-48d7-a563-427cd9f8604a

--- libzrtpcpp.spec.cmake
+++ libzrtpcpp.spec.cmake
@@ -27,7 +27,6 @@ Url:            http://www.gnu.org/software/commoncpp/commoncpp.html
 Source0:        %{name}-%{version}.tar.bz2
 Source1:        rpmlintrc
 BuildRoot:      %{_tmppath}/%{name}-%{version}-build
-Obsoletes:      %{name} < %{version}
 
 %description
 This library is a GPL licensed extension to the GNU RTP Stack, ccrtp,
