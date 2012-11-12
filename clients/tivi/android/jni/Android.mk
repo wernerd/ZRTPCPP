d82926f7aabf8a241bced54e6146dcb37942a2bc
Werner Dittmann <Werner.Dittmann@t-online.de>
2012-11-12 20:50:03 +0100

    TIVI - Change Android makefiles to build stati library

--- clients/tivi/android/jni/Android.mk
+++ clients/tivi/android/jni/Android.mk
@@ -1,32 +1,24 @@
 
-LOCAL_PATH := @CMAKE_SOURCE_DIR@
+LOCAL_PATH := /home/werner/silentC/zrtpcpp
 ROOT_SRC_PATH := $(LOCAL_PATH)
 
 #
-# Define and build the sqlite3 static lib
-#
-include $(CLEAR_VARS)
-LOCAL_MODULE := sqlite3
-LOCAL_SRC_FILES  := clients/tivi/android/jni/sqlite3/sqlite3.c
-include $(BUILD_STATIC_LIBRARY)
-
-#
-# Define and build the zrtpcpp shared lib
+# Define and build the zrtpcpp static lib
 #
 include $(CLEAR_VARS)
 LOCAL_MODULE := zrtpcpp
 LOCAL_CPP_FEATURES := @local_cpp_features@
 
 #
-# set to false if testing/compiling new modules to catch undefined symbols
-LOCAL_ALLOW_UNDEFINED_SYMBOLS := true
+# set to false if testing/compiling new modules to catch undefined symbols (if build shared lib without TIVI_ENV)
+# LOCAL_ALLOW_UNDEFINED_SYMBOLS := true
 
 # include paths for zrtpcpp modules
 LOCAL_C_INCLUDES += $(ROOT_SRC_PATH) $(ROOT_SRC_PATH)/srtp $(ROOT_SRC_PATH)/zrtp $(ROOT_SRC_PATH)/bnlib \
                     $(ROOT_SRC_PATH)/clients/tivi $(ROOT_SRC_PATH)/clients/tivi/android/jni/sqlite3
 
-LOCAL_CFLAGS := @TIVI_ENV@
+# LOCAL_CFLAGS := @TIVI_ENV@
+LOCAL_SRC_FILES  := clients/tivi/android/jni/sqlite3/sqlite3.c
 LOCAL_SRC_FILES += @zrtpcpp_src_spc@
 
-LOCAL_STATIC_LIBRARIES += sqlite3
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
--- clients/tivi/android/jni/Application.mk
+++ clients/tivi/android/jni/Application.mk
@@ -1,5 +1,4 @@
 # APP_PROJECT_PATH := /path/to/project
 # APP_PLATFORM := android-14
 APP_STL := gnustl_shared
-
-# APP_MODULES := sqlite3 zrtpcpp
+APP_MODULES := zrtpcpp
