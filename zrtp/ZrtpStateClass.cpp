d9e046095c5963e44e7d2175b6da32175c6e7303
Werner Dittmann <Werner.Dittmann@t-online.de>
2013-02-17 13:36:56 +0100

    Remove (comment) old GoClear code, not implemented

--- zrtp/ZrtpStateClass.cpp
+++ zrtp/ZrtpStateClass.cpp
@@ -1220,45 +1220,45 @@ void ZrtpStateClass::evWaitConfAck(void) {
 void ZrtpStateClass::evWaitClearAck(void) {
     DEBUGOUT((cout << "Checking for match in ClearAck.\n"));
 
-    char *msg, first, last, middle;
-    uint8_t *pkt;
-
-    if (event->type == ZrtpPacket) {
-	pkt = event->packet;
-	msg = (char *)pkt + 4;
-
-	first = tolower(*msg);
-    middle = tolower(*(msg+4));
-	last = tolower(*(msg+7));
-
-	/*
-	 * ClearAck:
-	 * - stop resending GoClear,
-	 * - switch to state AckDetected, wait for peer's Hello
-	 */
-	if (first == 'c' && middle == 'r' && last =='k') {
-	    cancelTimer();
-	    sentPacket = NULL;
-	    nextState(Initial);
-	}
-    }
-    // Timer event triggered - this is Timer T2 to resend GoClear w/o HMAC
-    else if (event->type == Timer) {
-        if (!parent->sendPacketZRTP(sentPacket)) {
-            sendFailed();                 // returns to state Initial
-            return;
-        }
-        if (nextTimer(&T2) <= 0) {
-            timerFailed(SevereTooMuchRetries);     // returns to state Initial
-        }
-    }
-    else {  // unknown Event type for this state (covers Error and ZrtpClose)
-        if (event->type != ZrtpClose) {
-            parent->zrtpNegotiationFailed(Severe, SevereProtocolError);
-        }
-	sentPacket = NULL;
-	nextState(Initial);
-    }
+//     char *msg, first, last, middle;
+//     uint8_t *pkt;
+// 
+//     if (event->type == ZrtpPacket) {
+// 	pkt = event->packet;
+// 	msg = (char *)pkt + 4;
+// 
+// 	first = tolower(*msg);
+//     middle = tolower(*(msg+4));
+// 	last = tolower(*(msg+7));
+// 
+// 	/*
+// 	 * ClearAck:
+// 	 * - stop resending GoClear,
+// 	 * - switch to state AckDetected, wait for peer's Hello
+// 	 */
+// 	if (first == 'c' && middle == 'r' && last =='k') {
+// 	    cancelTimer();
+// 	    sentPacket = NULL;
+// 	    nextState(Initial);
+// 	}
+//     }
+//     // Timer event triggered - this is Timer T2 to resend GoClear w/o HMAC
+//     else if (event->type == Timer) {
+//         if (!parent->sendPacketZRTP(sentPacket)) {
+//             sendFailed();                 // returns to state Initial
+//             return;
+//         }
+//         if (nextTimer(&T2) <= 0) {
+//             timerFailed(SevereTooMuchRetries);     // returns to state Initial
+//         }
+//     }
+//     else {  // unknown Event type for this state (covers Error and ZrtpClose)
+//         if (event->type != ZrtpClose) {
+//             parent->zrtpNegotiationFailed(Severe, SevereProtocolError);
+//         }
+// 	sentPacket = NULL;
+// 	nextState(Initial);
+//     }
 }
 
 
@@ -1362,7 +1362,7 @@ void ZrtpStateClass::evSecureState(void) {
         }
         /*
          * GoClear received, handle it. TODO fix go clear handling
-         */
+         *
         if (first == 'g' && last == 'r') {
             ZrtpPacketGoClear gpkt(pkt);
             ZrtpPacketClearAck* clearAck = parent->prepareClearAck(&gpkt);
@@ -1372,6 +1372,7 @@ void ZrtpStateClass::evSecureState(void) {
             }
         // TODO Timeout to resend clear ack until user user confirmation
         }
+        */
     }
     else if (event->type == Timer) {
         // Ignore stray timeout in this state
