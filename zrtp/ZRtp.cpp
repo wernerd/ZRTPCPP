5cef421caf29b7f6d086c58578938dd1834f4c62
Werner Dittmann <Werner.Dittmann@t-online.de>
2013-02-25 16:08:05 +0100

    Fix initialization of conf2Ack simulation event

--- zrtp/ZRtp.cpp
+++ zrtp/ZRtp.cpp
@@ -2426,7 +2426,8 @@ void ZRtp::conf2AckSecure() {
     Event_t ev;
 
     ev.type = ZrtpPacket;
-    ev.packet = (uint8_t*)&zrtpConf2Ack;
+    ev.packet = (uint8_t*)zrtpConf2Ack.getHeaderBase();
+    ev.length = sizeof (Conf2AckPacket_t) + 12;  // 12 is fixed ZRTP (RTP) header size
 
     if (stateEngine != NULL) {
         stateEngine->processEvent(&ev);
