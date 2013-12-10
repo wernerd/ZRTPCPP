5c1a64099197c96c5babf6a048f637853955a72d
Werner Dittmann <Werner.Dittmann@t-online.de>
2013-12-10 13:53:53 +0100

    Better names for the curves only, more consistent

--- zrtp/ZrtpConfigure.cpp
+++ zrtp/ZrtpConfigure.cpp
@@ -179,13 +179,13 @@ SymCipherEnum::~SymCipherEnum() {}
  */
 PubKeyEnum::PubKeyEnum() : EnumBase(PubKeyAlgorithm) {
     insert(dh2k, 0, "DH-2048", NULL, NULL, None);
-    insert(ec25, 0, "ECDH-256", NULL, NULL, None);
+    insert(ec25, 0, "NIST ECDH-256", NULL, NULL, None);
     insert(dh3k, 0, "DH-3072", NULL, NULL, None);
-    insert(ec38, 0, "ECDH-384", NULL, NULL, None);
-    insert(mult, 0, "Multi-stream", NULL, NULL, None);
+    insert(ec38, 0, "NIST ECDH-384", NULL, NULL, None);
+    insert(mult, 0, "Multi-stream",  NULL, NULL, None);
 #ifdef SUPPORT_NON_NIST
-    insert(e255, 0, "Curve25519", NULL, NULL, None);
-    insert(e414, 0, "Curve3617", NULL, NULL, None);
+    insert(e255, 0, "ECDH-255", NULL, NULL, None);
+    insert(e414, 0, "ECDH-414", NULL, NULL, None);
 #endif
 }
 
