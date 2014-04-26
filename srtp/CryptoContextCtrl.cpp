53acc6ed7d6875b59a28f7772b236ace7b2a55c4
Werner Dittmann <Werner.Dittmann@t-online.de>
2014-04-26 15:23:55 +0200

    Fix replay and ROC computations
    The wrong computations lead to false replay indications and to wrong
    HMAC, thus they dropped to much packets, in particular under bad network
    conditions.

--- srtp/CryptoContextCtrl.cpp
+++ srtp/CryptoContextCtrl.cpp
@@ -371,7 +371,7 @@ void CryptoContextCtrl::update(uint32_t index)
         replay_window |= 1;
     }
     else {
-        replay_window |= ( 1 << delta );
+        replay_window |= ( 1 << -delta );
     }
     if (index > s_l)
         s_l = index;
