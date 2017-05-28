9a0fa5921f6fb08fa9396529f7ec3025fea8ee2d
Werner Dittmann <Werner.Dittmann@t-online.de>
2017-05-28 12:05:32 +0200

    Delete context structure in ZrtpDH destructor, fixes a small memory leak (silence compiler warning)

--- zrtp/crypto/zrtpDH.cpp
+++ zrtp/crypto/zrtpDH.cpp
@@ -186,8 +186,8 @@ ZrtpDH::ZrtpDH(const char* type) {
 
     uint8_t random[64];
 
-    ctx = static_cast<void*>(new dhCtx);
-    dhCtx* tmpCtx = static_cast<dhCtx*>(ctx);
+    dhCtx* tmpCtx = new dhCtx;
+    ctx = static_cast<void*>(tmpCtx);
 
     // Well - the algo type is only 4 char thus cast to int32 and compare
     if (*(int32_t*)type == *(int32_t*)dh2k) {
@@ -289,7 +289,7 @@ ZrtpDH::~ZrtpDH() {
         ecFreeCurvesCurve(&tmpCtx->curve);
         break;
     }
-    delete ctx;
+    delete tmpCtx;
     ctx = nullptr;
 }
 
