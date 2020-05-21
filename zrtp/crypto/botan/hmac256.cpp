a608ab5bacfd4749ec02ecbc0630203a4f6c302e
werner <werner.dittmann@t-online.de>
2020-05-21 19:38:11 +0200

    Implement first modules to support botan and to replace old standalone modules

---
+++ zrtp/crypto/botan/hmac256.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2006 - 2018, Werner Dittmann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Authors: Werner Dittmann
+ */
+
+#include <cstdint>
+#include <cstring>
+#include <botan_all.h>
+#include "zrtp/crypto/hmac256.h"
+
+struct shaCtx {
+    std::unique_ptr<Botan::MessageAuthenticationCode> mac;
+};
+
+void hmac_sha256(const uint8_t *key, uint64_t keyLength, const uint8_t* data, uint64_t dataLength, zrtp::RetainedSecArray & macOut)
+{
+    auto hmac = Botan::MessageAuthenticationCode::create("HMAC(SHA-256)");
+
+    hmac->set_key(key, keyLength);
+    hmac->update(data, dataLength);
+    hmac->final(macOut.data());
+    macOut.size(SHA256_DIGEST_SIZE);
+}
+
+void hmacSha256(const uint8_t* key, uint64_t keyLength,
+                const std::vector<const uint8_t*>& dataChunks,
+                const std::vector<uint64_t>& dataChunkLength,
+                zrtp::RetainedSecArray & macOut)
+{
+    auto hmac = Botan::MessageAuthenticationCode::create("HMAC(SHA-256)");
+
+    hmac->set_key(key, keyLength);
+
+    for (size_t i = 0, size = dataChunks.size(); i < size; i++) {
+        hmac->update(dataChunks[i], dataChunkLength[i]);
+    }
+    hmac->final(macOut.data());
+    macOut.size(SHA256_DIGEST_SIZE);
+}
---
+++ zrtp/crypto/botan/sha256.cpp
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2006 - 2018, Werner Dittmann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author: Werner Dittmann
+ */
+
+#include <botan_all.h>
+#include <zrtp/crypto/sha256.h>
+
+struct shaCtx {
+    std::unique_ptr<Botan::HashFunction> hash;
+};
+
+void sha256(const uint8_t *data, uint64_t dataLength, uint8_t *digest )
+{
+    auto hash(Botan::HashFunction::create("SHA-256"));
+    hash->update(data, dataLength);
+    hash->final(digest);
+}
+
+void sha256(const std::vector<const uint8_t*>& data, const std::vector<uint64_t>& dataLength, uint8_t *digest)
+{
+    auto hash(Botan::HashFunction::create("SHA-256"));
+
+    for (size_t i = 0, size = data.size(); i < size; i++) {
+        hash->update(data[i], dataLength[i]);
+    }
+    hash->final(digest);
+}
+
+void* createSha256Context()
+{
+    auto *ctx = new shaCtx;
+    ctx->hash = Botan::HashFunction::create("SHA-256");
+    return (void*)ctx;
+}
+
+void closeSha256Context(void* ctx, uint8_t * digest)
+{
+    auto* hd = reinterpret_cast<shaCtx *>(ctx);
+
+    if (digest != nullptr && hd != nullptr) {
+        hd->hash->final(digest);
+    }
+    hd->hash.reset();
+    delete hd;
+}
+
+void* initializeSha256Context(void* ctx)
+{
+    auto* hd = reinterpret_cast<shaCtx *>(ctx);
+
+    if (hd != nullptr) {
+        hd->hash->clear();
+    }
+    return (void*)hd;
+}
+
+void finalizeSha256Context(void* ctx, zrtp::RetainedSecArray & digestOut)
+{
+    auto* hd = reinterpret_cast<shaCtx *>(ctx);
+    hd->hash->final(digestOut.data());
+    digestOut.size(SHA256_DIGEST_SIZE);
+}
+
+void sha256Ctx(void* ctx, const uint8_t* data, uint64_t dataLength)
+{
+    auto* hd = reinterpret_cast<shaCtx *>(ctx);
+
+    hd->hash->update(data, dataLength);
+}
+
+void sha256Ctx(void* ctx, const std::vector<const uint8_t*>& data, const std::vector<uint64_t>& dataLength)
+{
+    auto* hd = reinterpret_cast<shaCtx *>(ctx);
+
+    for (size_t i = 0, size = data.size(); i < size; i++) {
+        hd->hash->update(data[i], dataLength[i]);
+    }
+}
